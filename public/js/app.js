4ca6c23ef44c594008a4c467148b9ba57e8b2c4a
Richard <richardjalba@gmail.com>
2020-10-31 18:15:07 -0700

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -3,9 +3,6 @@ const search = document.querySelector('input');
 const messageOne = document.querySelector('#message-1');
 const messageTwo = document.querySelector('#message-2');
 
-messageOne.textContent = 'From JavaScript';
-messageTwo.textContent = 'From JavaScript';
-
 weatherForm.addEventListener('submit', (e) => {
   e.preventDefault();
 
@@ -14,16 +11,14 @@ weatherForm.addEventListener('submit', (e) => {
   messageOne.textContent = 'Loading...';
   messageTwo.textContent = '';
 
-  fetch('http://localhost:3000/weather?address=' + location).then(
-    (response) => {
-      response.json().then((data) => {
-        if (data.error) {
-          messageOne.textContent = data.error;
-        } else {
-          messageOne.textContent = data.location;
-          messageTwo.textContent = data.forecast;
-        }
-      });
-    }
-  );
+  fetch('/weather?address=' + location).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        messageOne.textContent = data.error;
+      } else {
+        messageOne.textContent = data.location;
+        messageTwo.textContent = data.forecast;
+      }
+    });
+  });
 });
